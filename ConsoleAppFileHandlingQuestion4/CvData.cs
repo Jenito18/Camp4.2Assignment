27fcbc9a3e72010d1738a5405557b2709b58dfb1
Jenito18 <113631472+Jenito18@users.noreply.github.com>
2024-08-13 00:04:45 +0530

    Add files via upload
    FileHandlingQuestion4

---
+++ ConsoleAppFileHandlingQuestion4/CvData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFileHandlingQuestion4
+{
+    
+    public class CvData
+    {
+        //fields to store values
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Qualification { get; set; }
+        public string Experience { get; set; }
+
+
+        //constructor
+        public CvData(string name, string location, string qualification, string experience)
+        {
+           this.Name = name;
+            this.Location = location;
+            this.Qualification = qualification;
+            this.Experience = experience;
+        }
+
+        // Method 
+        public string CVCreation()
+        {
+            return ($"Name: {Name}\n" )+
+                   ($"Location: {Location}\n") +
+                   ($"Qualification: {Qualification}\n") +
+                  ( $"Experience: {Experience}\n");
+        }
+    }
+}
---
+++ ConsoleAppFileHandlingQuestion4/Program.cs
@@ -0,0 +1,56 @@
+﻿using ConsoleAppFileHandlingQuestion4;
+
+namespace ConsoleAppFileHandlingQuestion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating list
+
+            List<CvData> Values = new List<CvData>();
+
+           
+            //getting input from user
+
+                Console.Write("Enter Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter Location: ");
+                string location = Console.ReadLine();
+
+                Console.Write("Enter Qualification: ");
+                string qualification = Console.ReadLine();
+
+                Console.Write("Enter Experience: ");
+                string experience = Console.ReadLine();
+
+                // Adding data to the list
+
+                Values.Add(new CvData(name, location, qualification, experience));
+            
+
+            // creating file path
+
+            string folderPath = "CVs";
+            Directory.CreateDirectory(folderPath);
+
+            // Generate a CV file for each entity
+            foreach (var Value in Values)
+            {
+                string fileName = $"{Value.Name}_{Value.Location}.txt";
+
+                string filePath = Path.Combine(folderPath, fileName);
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.Write(Value.CVCreation());
+                }
+
+                Console.WriteLine($"CV for {Value.Name} generated: {filePath}");
+            }
+
+            Console.WriteLine("\nAll CVs have been generated successfully.");
+        }
+    }
+}
